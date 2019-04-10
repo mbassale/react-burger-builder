3e95569fd85474382310e13471be9a0a8f71ca4e
Marco Bassaletti <bassaletti@gmail.com>
2019-04-10 00:37:02 -0400

    Display price over ingredients controls.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,6 +11,7 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={styles.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(control => (
             <BuildControl key={control.label} label={control.label}
                           added={() => props.ingredientAdded(control.type)}
