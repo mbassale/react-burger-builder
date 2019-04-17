bdd80fe4d8883c74d57e82f5efea91b5351e6871
Marco Bassaletti <bassaletti@gmail.com>
2019-04-17 01:13:18 -0400

    Created toolbar component.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import styles from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Auxiliary>
-        <div>Toolbar, Sidebar, Backdrop</div>
+        <Toolbar />
         <main className={styles.Content}>
             {props.children}
         </main>
