b92cffc4b1fd259d95d0d4594373a9b733ed8785
Marco Bassaletti <bassaletti@gmail.com>
2019-04-17 01:21:39 -0400

    Created logo component for toolbar.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logo from '../../Logo/Logo';
 import styles from './Toolbar.module.css';
 
 const toolbar = (props) => (
     <header className={styles.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>...</nav>
     </header>
 );
