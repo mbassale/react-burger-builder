61c1d3466a11d7331651eeb5f9d29a9771a4e9c8
Marco Bassaletti <bassaletti@gmail.com>
2019-04-29 08:31:09 -0400

    Checkout / Contact Data: submit order to database on contact data form.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -77,31 +77,11 @@ class BurgerBuilder extends Component {
     purchaseCancelHandler = () => this.setState({purchasing: false});
 
     purchaseContinueHandler = () => {
-        /*this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Marco Bassaletti',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '123456',
-                    country: 'Chile'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'Pickup'
-        };
-        axios.post('/orders.json', order).then(response => {
-            console.log(response);
-        }).catch(error => {
-            console.log(error);
-        }).finally(() => this.setState({loading: false, purchasing: false}));
-        */
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         this.props.history.push({
             pathname: '/checkout',
             search: '?' + queryParams.join('&')
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
 import styles from './ContactData.module.css';
 
@@ -9,7 +10,32 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
-        }
+        },
+        loading: false
+    };
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({loading: true});
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Marco Bassaletti',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '123456',
+                    country: 'Chile'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'Pickup'
+        };
+        axios.post('/orders.json', order).then(response => {
+            console.log(response);
+        }).catch(error => {
+            console.log(error);
+        }).finally(() => this.setState({loading: false}));
     };
 
     render() {
@@ -17,11 +43,11 @@ class ContactData extends Component {
             <div className={styles.ContactData}>
                 <h4>Entry Your Contact Data</h4>
                 <form>
-                    <input className={styles.Input} type="text" name="name" placeholder="Your name" />
-                    <input className={styles.Input} type="email" name="email" placeholder="Your email" />
-                    <input className={styles.Input} type="text" name="street" placeholder="Street Address" />
-                    <input className={styles.Input} type="text" name="postalCode" placeholder="Postal Code" />
-                    <Button btnType="Success">Order</Button>
+                    <input className={styles.Input} type="text" name="name" placeholder="Your name"/>
+                    <input className={styles.Input} type="email" name="email" placeholder="Your email"/>
+                    <input className={styles.Input} type="text" name="street" placeholder="Street Address"/>
+                    <input className={styles.Input} type="text" name="postalCode" placeholder="Postal Code"/>
+                    <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
                 </form>
             </div>
         );
