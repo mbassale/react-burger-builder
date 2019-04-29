03326c3fbc459af80d40ecdde852a0c6c13a8efc
Marco Bassaletti <bassaletti@gmail.com>
2019-04-29 08:49:27 -0400

    Orders: created dummy orders page with navlink and route.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './NavigationItem.module.css';
 
 const navigationItem = (props) => (
     <li className={styles.NavigationItem}>
-        <a href={props.link} className={props.active ? styles.active : null}>{props.children}</a>
+        <NavLink to={props.link} activeClassName={styles.active} exact>{props.children}</NavLink>
     </li>
 );
 
